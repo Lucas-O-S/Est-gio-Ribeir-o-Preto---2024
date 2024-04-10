9951654bad8ff9b6064d57f2838a1cfb08dcbd90
Lucas Oliveira Silva <127321390+Lucas-O-S@users.noreply.github.com>
2024-04-10 13:56:42 -0300

    Add files via upload

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma palavra: ");
+            string palavra = Console.ReadLine();
+
+            string reverso = "";
+
+            for(int i = palavra.Length-1; i >= 0;i--)
+            {
+                reverso += palavra[i];
+            }
+
+            Console.WriteLine($"Palavra invertida: {reverso}");
+
+            Console.ReadKey();
+        }
+    }
+}
