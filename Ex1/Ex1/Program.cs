c76929028bdb7cce8b52db67a38e05112758c9bb
Lucas Oliveira Silva <127321390+Lucas-O-S@users.noreply.github.com>
2024-04-10 13:08:49 -0300

    Add files via upload

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int indice = 13, soma = 0, k = 0;
+
+            while (k < indice)
+            {
+                k += 1;
+                soma += k;
+            }
+
+            Console.WriteLine(soma);
+            Console.ReadKey();
+        }
+    }
+}
